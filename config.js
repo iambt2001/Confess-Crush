a7a089098a9f15f243813c2971ed9958fb1b04a4
iambt2001 <94424155+iambt2001@users.noreply.github.com>
2021-11-16 13:54:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "QUÁ TAM BA BẬN",
+    introTitle: 'Hi, Bảo An',
+    introDesc: `Ánh mắt ta chạm nhau, chỉ muốn ngắm em lâu thật lâu :))))`,
+    btnIntro: 'Do em mở mắt to hơn BT thui :vvv',
+    title: 'Muốn mời food rì viu ờ đi ăn mà không biết có được không ????',
+    desc: 'Bán anh ít thời gian sau 20-11 của em nhé',
+    btnYes: 'OKeeee, em sẽ đi ăn với anh nè',
+    btnNo: 'KHÔNG, đấm cho dừ em đùa đó, không đi mô',
+    question: 'Em biết tại răng BT anh phải có BA không ????',
+    btnReply: 'Gửi câu trả lời',
+    reply: 'Vì có BA thì 2 năm sau mời đẹp đó :))))))',
+    mess: 'Tối nay anh call em lên lịch đi ăn nha',
+    messDesc: 'NĂN NỈ NĂN NỈ NĂN NỈ',
+    btnAccept: 'Okiiiii lun ',
+    messLink: 'https://www.pinterest.com/pin/855824735447809769/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
