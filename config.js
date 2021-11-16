5b48402700963a4f2369bcdb3b1dc1eb88d01234
iambt2001 <94424155+iambt2001@users.noreply.github.com>
2021-11-16 13:32:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 const CONFIG = {
     titleWeb: "Quá tam ba bận",
     introTitle: 'Bảo An',
-    introDesc: `Ánh mặt ta chạm nhau :)) 
-    Chỉ muốn ngắm em lâu thật lâu`,
+    introDesc: `Ánh mặt ta chạm nhau, chỉ muốn ngắm em lâu thật lâu :)))`,
     btnIntro: '^^Do em mở mắt to hơn BT thôi^^',
     title: 'Cho anh mua ít thời gian của em sau 20-11 nha ?',
     desc: 'Để mời food rì viu ờ đi ăn một bữa nè ',
     btnYes: 'OKEEE chốt luôn',
     btnNo: 'KHÔNG, em đùa chớ không đi mô :)))',
     question: 'Em biết tại răng BT anh phải có BA không ???',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi câu trả lời',
     reply: 'Vì có BA thì 2 năm sau mới đẹp <33333',
-    mess: 'Anh sẽ báo lịch em sau nha',
+    mess: 'Tối nay anh gọi lên lịch đi ăn nha',
     messDesc: 'Năn nỉ năn nỉ năn nỉ',
     btnAccept: 'Okeeee lunnn',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
